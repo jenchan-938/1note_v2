d5e317fd7b4ccf0cd1077d077d09f3fbcc02626f
jenchan-938 <jchan5@uchicago.edu>
2024-12-07 17:45:46 +0000

    chat_gpt_lesson

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -31,18 +31,55 @@ class DocumentsController < ApplicationController
   end
 
   def update
-    the_id = params.fetch("path_id")
-    the_document = Document.where({ :id => the_id }).at(0)
-    the_document.user_id = current_user.id
-    the_document.title = params.fetch("query_title")
+    reclassification = params.fetch("reclassify")
+    doc_id= params.fetch("path_id")
+    doc =Document.where({:id => doc_id}).at(0)
 
-    if the_document.valid?
-      the_document.save
-      redirect_to("/documents/#{the_document.id}", { :notice => "Document updated successfully."} )
-    else
-      redirect_to("/documents/#{the_document.id}", { :alert => the_document.errors.full_messages.to_sentence })
+    if reclassification == "on"
+
+      all_notes = [
+        {
+          "role" => "system",
+          "content" => "You are the world's best formatter. My goal is to reformat my notes to optimize clarity and insights. Can you please read through all of these notes I will provide and think about how to best show all of these notes on the page based on the content. [For example, if the note is about tracking to-do's make each note the same bullets. If the note is about fitness make sure to categorize it by muscle group/body part to help someone track their progress.] Please give me an output in HTML."
+        },
+    
+        {
+      "role" => "user",
+      "content" => "the notes are #{doc.notes.all}"
+      }
+      ]
+
+      # Send the message list to the API
+      client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_KEY"))
+
+      chatgpt_response = client.chat(
+        parameters: {
+          model: "gpt-3.5-turbo",
+          messages: all_notes
+        }
+      )
+
+      chat_gpt_reclassification= chatgpt_response.fetch("choices").at(0).fetch("message").fetch("content")
+      @chat_gpt_reclassify = chat_gpt_reclassification.html_safe
+          #need to create a new column and then need to set it to .save onto the doc.save
+          #lesson on active lesson > creating migration - adding or removing columns 
+          #contact_book
+          #when you redirect you had a query string - that value you get from - query string the next route is redirecting to params harsh and fetch it ;
+          
+      else
+        @chat_gpt_reclassify = "na"
+
+      end
+    
+
+      if doc.valid?
+        doc.save
+        redirect_to("/documents/#{doc.id}", { :notice => "Document updated successfully."} )
+        pp @chat_gpt_reclassify
+      else
+        redirect_to("/documents/#{doc.id}", { :alert => doc.errors.full_messages.to_sentence })
+      end
     end
-  end
 
   def destroy
     the_id = params.fetch("path_id")
--- chat.rb
+++ chat.rb
@@ -2,22 +2,15 @@ require "openai"
 require "dotenv/load"
 
 
-#where do I put this code?
-#I need to tell chatgpt the current list of doc names and body note >> how to tell it the body if the forum is not submitted yet?
-# set up the prompt structure  >> any feedback?
-# how to do if select then use user selected, if not then use the chat gpt? Do I had an if statement to the notes controller if id = blank? how to make it ok if we dont selete forum? Can i add on select to be on untitled?
-#how to integrate text file into the first page?
 
-# Set up the message list with a system message
-
-message_list = [
+all_notes = [
   {
     "role" => "system",
-    "content" => "You are the world's best categorizer. My goal is to categorize all of my notes into specific documents with different titles. I will provide you with a random note. The note is: #{}"
+    "content" => "You are the world's best formatter. My goal is to reformat my notes to optimize clarity and insights. Can you please read through all of these notes I will provide and think about how to best show all of these notes on the page based on the content. [For example, if the note is about tracking to-do's make each note the same bullets. If the note is about fitness make sure to categorize it by muscle group/body part to help someone track their progress.] Please give me an output in HTML. Please do not include <DOCUTYPE>, <body>, and just the core html."
   },
   {
     "role" => "user",
-    "content" => "Can you please help me assign this note with a title/category? Please either assign it to one of these document titles listed here: #{Document.all.pluck(:title)} or if the note does not fit any of these document titles please assign a new title that is one to two words"
+    "content" => "the notes are {Document.notes.all}"
   }
 ]
 
@@ -25,15 +18,15 @@ message_list = [
     # Send the message list to the API
     client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_KEY"))
 
-    api_response = client.chat(
+    chatgpt_response = client.chat(
       parameters: {
         model: "gpt-3.5-turbo",
-        messages: message_list
+        messages: all_notes
       }
     )
 
-   @chatgpt_title=api_response.fetch("choices").at(0).fetch("message").fetch("content")
+   @chat_gpt_reclassification =chatgpt_response.fetch("choices").at(0).fetch("message").fetch("content")
   
-   pp @chatgpt_title
+   
 
-   #when a user triggers a create action 
+   
